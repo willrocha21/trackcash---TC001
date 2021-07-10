ce0e7dbf632606e5c0116819ddfb757d6cfa6b0d
Willian Rocha <contato@ecorde.com.br>
2021-07-09 21:49:00 -0300

    segundo commit

---
+++ cadastro.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="pt">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Cadastro TrackCash</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet"> 
+		<link rel="stylesheet" href="css/estilo.css">
+	</head>
+	<body>
+		<div class="container-fluid padding-main">
+			<div class="row">
+				<div class="callFrame col-xs-12 col-sm-12 col-md-6 col-lg-6">
+					<img class="callFrameImg" src="/images/scott-graham-5fNmWej4tAA-unsplash1.jpg" alt="">
+					<h1 class="display-4">Muito mais que um conciliador financeiro!</h1>
+					<p class="lead">A melhor ferramenta no mercado e a única com processo automatizado, que compara as informações entre Plataformas, Marketplaces, Transportadoras, Meios de Pagamento e Instituições Financeiras!</p>
+				</div>
+				<div class="actionForm grayBg col-xs-12 col-sm-12 col-md-6 col-lg-6">
+					<div class="row">
+						<div class="centerElement col-xs-12 col-sm-12 col-md-6 col-lg-7">
+							<figure class="figure">
+								<img src="/images/logo-trackcash.png" alt="">
+							</figure>
+						</div>
+					</div>
+					<div class="row">
+						<div class="centerElement col-xs-12 col-sm-12 col-md-6 col-lg-7">
+							<h2>Cadastre-se no TrackCash:</h2>
+						</div>
+					</div>	
+					<div class="row">
+						<div class="centerElement col-xs-12 col-sm-12 col-md-6 col-lg-7">
+							<form action="" method="post">
+							  <div class="form-group">
+							    <input type="text" name="cpfCnpj" class="form-control" id="cpfCnpj" placeholder="CPF/CNPJ">
+							  </div>
+							  <div class="form-group">
+							    <input type="text" name="nome" class="form-control" id="nome" placeholder="Nome">
+							  </div>							  								
+							  <div class="form-group">
+							    <input type="email" class="form-control" id="email" placeholder="Email">
+							  </div>
+							  <div class="form-group">
+							    <input type="tel" class="form-control" id="telefone" placeholder="Telefone">
+							  </div>							  
+							  <div class="form-group">
+							    <input type="password" class="form-control" id="senha" placeholder="Senha">
+							  </div>
+							  <div class="form-group">
+							    <input type="password" class="form-control" id="senhaConfirme" placeholder="Confirme sua senha">
+							  </div>							  
+							  <button type="submit" class="btn btn-primary w-100">Acessar o Sistema</button>
+							</form>
+						</div>
+					</div>
+					<div class="row">
+						<div class="centerElement col-xs-12 col-sm-12 col-md-6 col-lg-7">
+							<a href="login.php">Já possui uma conta? Faça login!</a>
+						</div>
+					</div>
+				</div>				
+			</div>
+		</div>
+
+	    <!-- Optional JavaScript -->
+	    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>	
+	</body>
+
+</html>
